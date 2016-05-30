c925f4fcb04951fb20bb26206886b05b7a7b574a
Maurizio <maurizio.troiani94@gmail.com>
2016-05-30 16:50:48 +0200

    Initial commit

---
+++ app/src/main/java/ddt/sms16/ivu/di/uniba/it/easycar/MainActivity.java
@@ -0,0 +1,13 @@
+package ddt.sms16.ivu.di.uniba.it.easycar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/test/java/ddt/sms16/ivu/di/uniba/it/easycar/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package ddt.sms16.ivu.di.uniba.it.easycar;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
