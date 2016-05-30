d9a9968d6e623a3d708de0b86ff7792546923d24
RedEyes94 <Gdemarzo94@gmail.com>
2016-05-30 18:27:00 +0200

    commit tj

--- app/src/main/java/ddt/sms16/ivu/di/uniba/it/easycar/MainActivity.java
+++ app/src/main/java/ddt/sms16/ivu/di/uniba/it/easycar/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Questo è per d'Elia
+        //Questo è per TTTJJJ
     }
 }
