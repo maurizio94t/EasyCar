6cb58807fa6e751d8b54567540ea4407b5a4f5ad
Enrico <enrico.marzo94@gmail.com>
2016-05-30 17:34:25 +0200

    Prova Enrico

--- app/src/main/java/ddt/sms16/ivu/di/uniba/it/easycar/MainActivity.java
+++ app/src/main/java/ddt/sms16/ivu/di/uniba/it/easycar/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Questo è per Delia
+        //Questo è per Peppinoooo
     }
 }
