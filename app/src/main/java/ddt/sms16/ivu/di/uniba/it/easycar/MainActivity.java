b56fead1a6f7b596fe4f9e01b261b26c64d25e07
Maurizio <Maurizio>
2016-06-01 17:25:46 +0200

    Inserita un'activity che permette il caricamento prima di entrare nell'activity con il navigation drawer

--- app/src/main/java/ddt/sms16/ivu/di/uniba/it/easycar/MainActivity.java
+++ app/src/main/java/ddt/sms16/ivu/di/uniba/it/easycar/MainActivity.java
@@ -1,22 +1,11 @@
 package ddt.sms16.ivu.di.uniba.it.easycar;
 
-import android.app.Fragment;
-import android.app.FragmentTransaction;
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -25,10 +14,7 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-import ddt.sms16.ivu.di.uniba.it.easycar.fragments.HomeFragment;
-import ddt.sms16.ivu.di.uniba.it.easycar.fragments.OneFragment;
-
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity {
 
     // URL to get data JSON
     private static String url = "http://t2j.no-ip.org/ddt/WebService.php";
@@ -49,106 +35,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         setContentView(R.layout.activity_main);
 
         new GetUsers().execute();
-
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-
-        // Faccio partire il primo Fragment
-        //FragmentTransaction ft = getFragmentManager().beginTransaction();
-        //ft.replace(R.id.fragment_container, new HomeFragment());
-        //ft.commit();
-
-        //navigationView.getMenu().getItem(0).setChecked(true);
-    }
-
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        Fragment fragment = null;
-        boolean ok = false;
-
-        int id = item.getItemId();
-        if (id == R.id.nav_home) {
-            fragment = new HomeFragment();
-            ok = true;
-        } else if (id == R.id.nav_one) {
-            fragment = new OneFragment();
-            ok = true;
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
-
-        if(ok) {
-            FragmentTransaction ft = getFragmentManager().beginTransaction();
-            ft.replace(R.id.fragment_container, fragment);
-            ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
-            ft.addToBackStack(null);
-            ft.commit();
-        }
-
-        item.setChecked(true);
-        setTitle(item.getTitle());
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
     }
 
     // AsynkTask
@@ -191,15 +77,10 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
              * Updating received data from JSON into ListView
              * */
 
-            /*  AL MOMENTO NON ABBIAMO QUI LA LISTA
-            ListAdapter adapter = new SimpleAdapter(
-                    MainActivity.this, listaUtenti,
-                    R.layout.list_item,
-                    new String[]{TAG_UTENTE_NOME, TAG_UTENTE_COGNOME, TAG_UTENTE_EMAIL},
-                    new int[]{R.id.nome, R.id.cognome, R.id.email});
-
-            setListAdapter(adapter);
-            */
+            Intent intent = new Intent(MainActivity.this, BaseActivity.class);
+            //intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_TASK_ON_HOME);
+            startActivity(intent);
+            finish();
         }
     }
 
